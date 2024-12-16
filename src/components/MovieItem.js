fa7e079e987f457aae65fcfc219e35747d7564a0
nahyun <knahyuk@gmail.com>
2024-12-16 11:59:21 +0900

    리액트에서 컴포넌트 재사용성 높이기

---
+++ src/components/MovieItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MovieItem = ({ title, img, children }) => {
+  return (
+    <div className="movie-item">
+      <img src={img} alt={title} />
+      <h3 className="movie-title">{title}</h3>
+      <div className="movie-details">{children}</div>
+    </div>
+  );
+};
+
+export default MovieItem;
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { API_KEY, BASE_URL } from "../config";
 
 import React from "react";
+import MovieItem from "./MovieItem";
+import "./MovieList.css";
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
@@ -10,37 +12,37 @@ const MovieList = () => {
   const IMG_URL = "https://image.tmdb.org/t/p/w200";
 
   useEffect(() => {
-    
-		fetchMovieData();
+    fetchMovieData();
   }, []);
 
-	const fetchMovieData = async () => {
-		try {
-			const response = await fetch(API_URL);
-			const data = await response.json();
-
-			setMovies(data.results);
-		} catch (error) {
-			// 에러 표시 보여주는 코드 작성 예정
-		}
-	};
+  const fetchMovieData = async () => {
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      console.log(data.results);
+      setMovies(data.results);
+    } catch (error) {
+      // 에러 표시 보여주는 코드 작성 예정
+    }
+  };
 
   return (
-		<div>
-			<h1>Popular MovieList</h1>
-			<div className="movie-container">
-				{movies.map((movie) => (
-					<div key={movie.id} className="movie-item">
-						<img 
-							src={`${IMG_URL}${movie.poster_path}`}
-							alt={movie.title}
-						/>
-						<h3>{movie.title}</h3>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+    <div>
+      <h1>Popular MovieList</h1>
+      <div className="movie-container">
+        {movies.map((movie) => (
+          <MovieItem
+            key={movie.id}
+            title={movie.title}
+            img={`${IMG_URL}${movie.poster_path}`}
+          >
+            <p>{movie.release_date}</p>
+            <p>{movie.vote_average} / 10</p>
+          </MovieItem>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MovieList;
